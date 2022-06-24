cd6b977d7f56620ce6f176247a1bff71f0448ca9
arnfinn <arnfinn.steindal@gmail.com>
2022-06-24 09:13:11 +0200

    Ratelimit requests

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import compression from "compression";
 import helmet from "helmet";
 import cors from "cors";
 import * as CMS from "./controllers/CMS";
+import rateLimit from "express-rate-limit";
 
 //router
 import registerInfoRouter from "./routes/info";
@@ -18,6 +19,15 @@ import * as TuName from "./controllers/TuName";
 const PORT = process.env.PORT ?? 4000;
 
 const app = express();
+
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(rateLimiter);
 
 // Adds a nonce to response for use on inline scripts
 app.use((req, res, next) => {
