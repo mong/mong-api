906ed09c028d88c06b8a1c8717f878fa14ca9d89
arnfinn <arnfinn.steindal@gmail.com>
2022-06-29 16:12:46 +0200

    extract medfields from db

--- src/controllers/info/medField.ts
+++ src/controllers/info/medField.ts
@@ -4,7 +4,22 @@ import { medField } from "../../models/info";
 export const medicalFields: RequestHandler = async (_, res) => {
   try {
     const rows = await medField();
-    res.json(rows);
+    const emptyArray: {shortName?: string,
+    name?: string,
+    registers?: string[]}[] = []
+
+    const testvalue = rows.reduce((prevVal, currVal) => {
+      const returnValue = prevVal.some(val => val.shortName === currVal.shortName) ?
+      prevVal.filter(val => val.shortName === currVal.shortName).map(val => val.registers?.push(currVal.registers)) : 
+      [...prevVal, {
+        shortName: currVal.shortName,
+        name: currVal.name,
+        registers: [currVal.registers],
+      }];
+      return returnValue;
+    }, emptyArray);
+
+    res.json(testvalue);
   } catch (err) {
     const typed_err = err as any;
     res.status(500).json({ message: typed_err.message });
